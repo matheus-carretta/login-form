6c60ff750e983064d85bed388f7a45745283ecd5
matheus-carretta <76985963+matheus-carretta@users.noreply.github.com>
2022-01-14 00:48:16 -0300

    Estrutura do form criada

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import welcome from './assets/welcome.svg';
+
 function App() {
   return (
     <div className="container">
-      Hello World!
+      <div className="container-login">
+        <div className="wrapper-login">
+          <form className="login-form">
+            <span className="login-form-title">Bem vindo!</span>
+            <span className="login-form-image">
+              <img src={ welcome } alt="Vem" />
+            </span>
+
+            <div className="wrapper-inputs">
+              <input type="email" />
+              <span className="focus-input" data-placeholder='Email'></span>
+            </div>
+
+            <div className="wrapper-inputs">
+              <input type="email" />
+              <span className="focus-input" data-placeholder='Email'></span>
+            </div>
+
+            <div className="container-login-form-btn">
+              <button className="login-form btn">Login</button>
+            </div>
+
+            <div className="create-account">
+              <span className="text">Não possui conta?</span>
+              <a href="#" className="text-2">Criar conta.</a>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
